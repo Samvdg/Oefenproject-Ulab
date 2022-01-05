1ec346b5456cbb68001ccade93961e03822786a5
Samvdg <samvdgreft@gmail.com>
2022-01-05 14:11:49 +0100

    - added extension 'transformer' description - fixed input macro - deleted blade file

--- app/Lib/Twigbridge/Extensions.php
+++ app/Lib/Twigbridge/Extensions.php
@@ -17,6 +17,15 @@ class Extensions extends AbstractExtension
         ];
     }
 
+    /**
+     * @param $key
+     * @return array|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Translation\Translator|string|null
+     *
+     * This function gets the translation of certain scentences.
+     * The translations are stored in a specific folder structure based on the current route
+     * Because of this, to get the correct translation, we combine the route and the key of the word/sentence that needs to be translated
+     *
+     */
     private function transformer($key)
     {
         return trans(request()->path().".$key");
