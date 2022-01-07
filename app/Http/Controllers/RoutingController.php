6acbec409dda0a89e5ebf1ab97bfa24c207b8f0b
Samvdg <samvdgreft@gmail.com>
2022-01-07 15:50:23 +0100

    - Added routing error handling   - will now give a customizable 404 page on wrong routes   - Still need to learn how to customize these

--- app/Http/Controllers/RoutingController.php
+++ app/Http/Controllers/RoutingController.php
@@ -9,59 +9,57 @@ class RoutingController extends BaseController
 {
     public function dynamic($uri, Request $request)
     {
+        // Start of the controller location string and get the request segments
         $controller = '\App\Http\Controllers\\';
         $seg = request()->segments();
-        $controller .= ucfirst($seg[0]) . '\\' . ucfirst($seg[1]) . 'Controller';
-        $controller = new $controller;
+        // Check if the given URI has enough segments. If not, return an error
+        if(count($seg) < 2) abort(404);
 
+        // Concatinate the controller string with the first 2 segments of the URI
+        $controller .= ucfirst($seg[0]) . '\\' . ucfirst($seg[1]) . 'Controller';
+
+        // Check if generated class even exists. If not return an error
+        if (class_exists($controller)) $controller =  new $controller;
+        else abort(404);
 
         switch (request()->method())
         {
             case 'GET':
 
                 // if there are only 2 segments on a get, it's the index.
-                if (count($seg) < 3){
-                    return $controller->index();
-                }
+                if (count($seg) < 3) return $controller->index();
 
                 // in case of 3 segments, the 3rd segment will be the id of an object
-                elseif (count($seg) == 3)
-                {
-                    return $controller->show($seg[2]);
-                }
+                elseif (count($seg) == 3) return function_exists($controller->show()) ? $controller->show($seg[2]) :  abort(404);
+
 
                 // incase of more segments, 4 to be exact, it will be edit or a different function defined in a controller
                 elseif (count($seg) > 3)
                 {
-                    if ($seg[3] == "back") {
-                        return redirect("$seg[0]/$seg[1]");
-                    }
-                    return $controller->{$seg[3]}($seg[2]);
+                    // If someone wants to go back, go back to the index of the object
+                    if ($seg[3] == "back") return redirect("$seg[0]/$seg[1]");
+                    else return $controller->{$seg[3]}($seg[2]);
                 }
-                break;
+
+                // I have no clue how you would get to this point but if you do you deserve a cookie
+                else return abort(404);
 
             case 'POST':
                 // If the request is a post with less then 3 segments, its a create
-                if (count($seg) < 3){
-                    return $controller->store($request);
-                }
+                if (count($seg) < 3) return $controller->store($request);
 
                 // Post with 3 items most likely means a delete
-                elseif (count($seg) == 3)
-                {
-                    return $controller->destroy($seg[2]);
-                }
+                elseif (count($seg) == 3) return $controller->destroy($seg[2]);
 
                 // Incase of more than 3 segments, there is most likely a custom function in the controller
-                elseif (count($seg) > 3)
-                {
-                    return $controller->{$seg[3]}(request(),$seg[2]);
-                }
-                // if there are 3 segments, it is a get for a controller action
+                elseif (count($seg) > 3) return $controller->{$seg[3]}(request(),$seg[2]);
+
+                // Very hard to get to 404
+                else abort(404);
 
                 break;
         }
 
-        return  "No clue where ur goin but it aint the right way";
+        return  abort(404);
     }
 }
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>404 Custom Error Page Example</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+</head>
+
+<body>
+<div class="container mt-5 pt-5">
+    <div class="alert alert-danger text-center">
+        <h2 class="display-3">404</h2>
+        <p class="display-5">Oops! Something is wrong.</p>
+    </div>
+</div>
+</body>
+
+
+@extends('errors::minimal')
+
+@section('title', __('Not Found'))
+@section('code', '404')
+
+@if($exception->getMessage())
+    @section('message', $exception->getMessage())
+@else
+    @section('message', __('Not Found'))
+@endif
+</html>
